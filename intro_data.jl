efbfaf2073fe7d30e85f062bd903dbedd4c87e7d
BenjaminTWP <benjamintwpettersson@gmail.com>
2024-10-07 14:00:23 +0200

    Theta and voltage

--- intro_data.jl
+++ intro_data.jl
@@ -13,3 +13,11 @@ cost_generator = [175, 100, 150, 150, 300, 350, 400, 300, 200]
 
 n_consumers = 7
 consumer_demand = [0.10, 0.19, 0.11, 0.09, 0.21, 0.05, 0.04]
+
+n_theta = 11
+lb_theta = [-4 for n=1:n_theta]
+ub_theta = [4 for n=1:n_theta]
+
+n_voltage = 11
+lb_volt = [0.98 for n=1:n_voltage]
+ub_volt = [1.02 for n=1:n_voltage]
