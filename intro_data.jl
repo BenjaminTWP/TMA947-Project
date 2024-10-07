0651b017ecbc1dc018f9f2f60100d58db9d76812
BenjaminTWP <benjamintwpettersson@gmail.com>
2024-10-07 13:40:46 +0200

    generators

--- intro_data.jl
+++ intro_data.jl
@@ -1,7 +1,10 @@
 # Small data file to show how one can import julia files into other julia files.
 # It, e.g., gives a convenient way to create files containing all the data for a problem.
 
-n_vars = 2 # Number of variables
+n_vars = 9 # Number of variables
 lb = [-1, -4] # Lower bounds for the varaibles
 ub = [3, 0] # Upper bounds for the variable
 sum_bound = 0.49 # Constraint on sum of variables
+
+n_generators = 9
+lb_generators = [0, 0, 0, 0, 0, 0, 0, 0, 0]
