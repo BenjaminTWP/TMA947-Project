bb7858fb44018b0595d3b814c9a36642637c2b9a
vistca <93658028+vistca@users.noreply.github.com>
2024-10-10 08:44:57 +0200

    objectiv added but not the constraints

--- energy_optimization.jl
+++ energy_optimization.jl
@@ -2,17 +2,23 @@ using JuMP, Ipopt
 model = Model(Ipopt.Optimizer)
 include("intro_data.jl")
 
-# Phase constraints
-@variable(model, z[i=1:11], lower_bound = -π, upper_bound = π)
-@variable(model, lb_volt[j] <= voltage[j=1:11] <= ub_volt[j])
+#### CONSTRAINTS
+@variable(model, lb_phase[k] <= phase[k=1:11] <= ub_phase[k])
+@variable(model, lb_volt[k] <= voltage[k=1:11] <= ub_volt[k])
 @variable(model, lb_generators[k] <= generator[k=1:n_generators] <= up_generators[k])
+@variable(model, consumer_demand[k=1:n_consumers]) # TODO Better to have this as lower bound?
 
+
+# Obkective function is the sum of the cost of producing the electricity in each generator
+@NLobjective(model, Min, sum(cost_generator[k] * generator[k] for k in 1:n_generators))
+
+
+#@NLconstraint()
+
+
+println(model)
 
 test = [voltage[3], voltage[4]]
-println(test)
+# println(test)
 
-#@NLconstraint(model, SOS_constr, sum(x[i]^2 for i in 1:n_vars) <= sum_bound)
-
-messages = "helloworld"
-
-println(messages)
+# @NLconstraint(model, SOS_constr, sum(x[i]^2 for i in 1:n_vars) <= sum_bound)
--- intro_data.jl
+++ intro_data.jl
@@ -7,7 +7,6 @@ ub = [3, 0] # Upper bounds for the variable
 sum_bound = 0.49 # Constraint on sum of variables
 
 neighbours = [[2,11], [1,3,11], [2,4,9], [3,5],[4,6,8], [5,7], [6,8,9], [5,7,9], [3,7,8,10], [9,11], [1,2,10]]
-display(neighbours)
 
 n_generators = 9
 lb_generators = [0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -18,9 +17,30 @@ n_consumers = 7
 consumer_demand = [0.10, 0.19, 0.11, 0.09, 0.21, 0.05, 0.04]
 
 n_theta = 11
-lb_theta = [-4 for n=1:n_theta]
-ub_theta = [4 for n=1:n_theta]
+lb_phase = [-pi for n=1:n_theta]
+ub_phase = [pi for n=1:n_theta]
 
 n_voltage = 11
 lb_volt = [0.98 for n=1:n_voltage]
 ub_volt = [1.02 for n=1:n_voltage]
+
+# Define the number of nodes
+n = 11
+
+# Initialize B and G matrices with zeros
+B = zeros(n, n)
+G = zeros(n, n)
+
+# Populate B and G based on the given edges and coefficients
+edges = [(1, 2), (1, 11), (2, 3), (2, 11), (3, 4), (3, 9), (4, 5), (5, 6), (5, 8), (6, 7), (7, 8), (7, 9), (8, 9), (9, 10), (10, 11)]
+b_coeffs = [-20.1, -22.3, -16.8, -17.2, -11.7, -19.4, -10.8, -12.3, -9.2, -13.9, -8.7, -11.3, -7.7, -13.5, -26.7]
+g_coeffs = [4.12, 5.67, 2.41, 2.78, 1.98, 3.23, 1.59, 1.71, 1.26, 1.11, 1.32, 2.01, 4.41, 2.14, 5.06]
+
+# Assign coefficients to corresponding locations in the matrices
+for ((k, l), b, g) in zip(edges, b_coeffs, g_coeffs)
+    B[k, l] = b
+    G[k, l] = g
+    # Assuming undirected network, symmetric matrix
+    B[l, k] = b
+    G[l, k] = g
+end
