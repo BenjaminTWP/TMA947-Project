060c7af7ed090288fdf94066a80c2a967c886b01
vistca <93658028+vistca@users.noreply.github.com>
2024-10-14 13:40:58 +0200

    added a plot of the energy flow between nodes
    Co-authored-by: Gustav Gille <it-like@users.noreply.github.com>

--- energy_optimization.jl
+++ energy_optimization.jl
@@ -45,4 +45,30 @@ println("Reactive")
 println("Optimal(?) point: ", JuMP.value.(reactive))
 
 
+println("Active power:")
+for n in 1:n_len
+    println("   Flows from node $n")
+    for nb in neighbours[n]
+        net_flow = 
+                - sum(JuMP.value.(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb]))) # Incoming FROM NB TO N
+                + sum(JuMP.value.(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[nb] * B[nb, n] * sin(θ[nb] - θ[n]))) # Outgoing FROM N TO NB
+        println("From node $n to node $nb the net flow is $net_flow")
+    end
+end
+
+println("")
+
+println("Reactive power:")
+for n in 1:n_len
+    println("   Flows from node $n")
+    for nb in neighbours[n]
+        net_flow = 
+            - sum(JuMP.value.(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb]))) # Incoming FROM NB TO N
+            + sum(JuMP.value.(-v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[nb] * G[nb, n] * sin(θ[nb] - θ[n]))) # Outgoing FROM N TO NB
+        println("From node $n to node $nb the net flow is $net_flow")
+    end
+end
+
+
+
 
