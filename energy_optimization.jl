4c44aabc063e1d53637eef28417c7fdf20e729a0
Gille <93673070+it-like@users.noreply.github.com>
2024-10-15 09:42:40 +0200

    Add tests to see net flow to ensure correct results

--- energy_optimization.jl
+++ energy_optimization.jl
@@ -8,24 +8,24 @@ include("intro_data.jl")
 @variable(model, lb_generators[k] <= generator[k=1:n_generators] <= ub_generators[k])
 @variable(model, lb_reactive[k] <= reactive[k=1:n_generators] <= ub_reactive[k])
 
-# Objective function is the sum of the cost of producing the electricity in each generator
 @NLobjective(model, Min, sum(cost_generator[k] * generator[k] for k in 1:n_generators))
 
-# n  = k
-# nb = l
-@NLconstraint(model, activePower[n = 1:n_len], 
-                (sum(generator[index] for index in generators_index[n]) # Generators
-                + sum(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb]) for nb in neighbours[n]) # Incoming FROM NB TO N
-                - sum(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * B[nb, n] * sin(θ[nb] - θ[n]) for nb in neighbours[n]) # Outgoing FROM N TO NB
-                - sum(consumer_demand[index] for index in consumers_index[n])) # Consumed 
-                == 0)
+
+@NLconstraint(model, activePower[n = 1:n_len],
+                 (sum(generator[index] for index in generators_index[n]) # Generators
+                 + sum(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb]) for nb in neighbours[n]) # Incoming FROM NB TO N (neighbours!!?!?!?!)
+                 - sum(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * B[nb, n] * sin(θ[nb] - θ[n]) for nb in neighbours[n]) # Outgoing FROM N TO NB
+                 - sum(consumer_demand[index] for index in consumers_index[n])) # Consumed 
+                 == 0)
     
 @NLconstraint(model, reactivePower[n = 1:n_len], 
                 (sum(reactive[index] for index in generators_index[n]) # Generators
-                + sum(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb]) -(v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * G[nb, n] * sin(θ[nb] - θ[n])) for nb in neighbours[n])) # Outgoing FROM N TO NB
-                == 0)   
+                + sum(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb]) for nb in neighbours[n]) # Incoming FROM NB TO N
+                - sum(-v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * G[nb, n] * sin(θ[nb] - θ[n]) for nb in neighbours[n])) # Outgoing FROM N TO NB
+                == 0)
 
 #println(model)
+
 
 
 # Solve the optimization problem
@@ -44,48 +44,49 @@ println("Optimal(?) point: ", JuMP.value.(θ))
 println("Reactive")
 println("Optimal(?) point: ", JuMP.value.(reactive))
 
-global net_flow = 0  # Initialize net_flow globally if it's used outside the loop as well.
+
+
+####
+#
+# TESTS
+#
+###
+global net_flow = 0
 println("Active power:")
 for n in 1:n_len
-    local net_flow_n = 0  # Initialize a local running sum for each node n.
-    println("   Flows from node $n")
+    local net_flow_n = 0 
+    #println("   Flows from node $n")
     for nb in neighbours[n]
         local flow = sum(JuMP.value.(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb])))
         flow -= sum(JuMP.value.(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * B[nb, n] * sin(θ[nb] - θ[n])))
 
-        # Update the local sum for this node
+       
         net_flow_n += flow
-        println("From node $n to node $nb the net flow is $flow")
+        #println("From node $n to node $nb the net flow is $flow")
     end
     global net_flow += net_flow_n  # Update the global running sum
-    println("Net flow for node $n: $net_flow_n")
+    #println("Net flow for node $n: $net_flow_n")
 end
-println("Total net flow: $net_flow")  # Display the total net flow
+println("Total active net flow: $net_flow") 
 
-#=
 
-println("Active power:")
-for n in 1:n_len
-    println("   Flows from node $n")
-    for nb in neighbours[n]
-        net_flow = 
-                - sum(JuMP.value.(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb]))) # Incoming FROM NB TO N
-                + sum(JuMP.value.(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[nb] * B[nb, n] * sin(θ[nb] - θ[n]))) # Outgoing FROM N TO NB
-        println("From node $n to node $nb the net flow is $net_flow")
-    end
-end
-
-println("")
-
+global net_flow = 0  
 println("Reactive power:")
 for n in 1:n_len
-    println("   Flows from node $n")
+    local net_flow_n = 0 
+    #println("   Flows from node $n")
     for nb in neighbours[n]
-        net_flow = 
-            - sum(JuMP.value.(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb]))) # Incoming FROM NB TO N
-            + sum(JuMP.value.(-v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[nb] * G[nb, n] * sin(θ[nb] - θ[n]))) # Outgoing FROM N TO NB
-        println("From node $n to node $nb the net flow is $net_flow")
+        local flow = sum(JuMP.value.(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb]))) # Reactive power flowing from n to nb
+        flow -= sum(JuMP.value.(-v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * G[nb, n] * sin(θ[nb] - θ[n]))) # Reactive power flowing from n to nb
+    
+    
+        net_flow_n += flow
+       # println("From node $n to node $nb the net flow is $flow")
     end
+    global net_flow += net_flow_n  
 end
+println("Total reactive net flow: $net_flow") 
 
-=#
+
+
+
