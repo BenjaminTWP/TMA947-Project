9d6eb207025f7be39c17bb0c455b4a6ca17a72c4
vistca <93658028+vistca@users.noreply.github.com>
2024-10-18 12:53:43 +0200

    reactive power is working

--- energy_optimization.jl
+++ energy_optimization.jl
@@ -13,16 +13,16 @@ include("intro_data.jl")
 
 
 @NLconstraint(model, activePower[n = 1:n_len], 
-                (sum(generator[index] for index in generators_index[n]) # Generators
-                + sum(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb]) for nb in neighbours[n]) # Incoming FROM NB TO N
-                - sum(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * B[nb, n] * sin(θ[nb] - θ[n]) for nb in neighbours[n]) # Outgoing FROM N TO NB
-                - sum(consumer_demand[index] for index in consumers_index[n])) # Consumed 
+                (sum(generator[index] for index in generators_index[n]; init=0) # Generators
+                - sum(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb]) for nb in neighbours[n]) # Outgoing FROM N TO NB
+                + sum(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * B[nb, n] * sin(θ[nb] - θ[n]) for nb in neighbours[n]) # Incoming FROM NB TO N
+                - sum(consumer_demand[index] for index in consumers_index[n]; init=0)) # Consumed 
                 == 0)
     
 @NLconstraint(model, reactivePower[n = 1:n_len], 
-                (sum(reactive[index] for index in generators_index[n]) # Generators
-                + sum(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb]) for nb in neighbours[n]) # Incoming FROM NB TO N
-                - sum(-v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * G[nb, n] * sin(θ[nb] - θ[n]) for nb in neighbours[n])) # Outgoing FROM N TO NB
+                (sum(reactive[index] for index in generators_index[n]; init=0) # Generators
+                - sum(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb]) for nb in neighbours[n]) # Outgoing FROM N TO NB
+                + sum(-v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * G[nb, n] * sin(θ[nb] - θ[n]) for nb in neighbours[n])) # Incoming FROM NB TO N
                 == 0)
 
 #println(model)
@@ -50,8 +50,8 @@ for n in 1:n_len
     println("   Flows from node $n")
     for nb in neighbours[n]
         net_flow = 
-                - sum(JuMP.value.(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb]))) # Incoming FROM NB TO N
-                + sum(JuMP.value.(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * B[nb, n] * sin(θ[nb] - θ[n]))) # Outgoing FROM N TO NB
+                JuMP.value.(v[n]^2 * G[n, nb] - v[n] * v[nb] * G[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * B[n, nb] * sin(θ[n] - θ[nb])) #  FROM N TO NB
+                - JuMP.value.(v[nb]^2 * G[nb, n] - v[nb] * v[n] * G[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * B[nb, n] * sin(θ[nb] - θ[n])) #  FROM NB TO N
         println("From node $n to node $nb the net flow is $net_flow")
     end
 end
@@ -62,13 +62,19 @@ println("Reactive power:")
 for n in 1:n_len
     println("   Flows from node $n")
     for nb in neighbours[n]
-        net_flow = 
-            - sum(JuMP.value.(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb]))) # Incoming FROM NB TO N
-            + sum(JuMP.value.(-v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * G[nb, n] * sin(θ[nb] - θ[n]))) # Outgoing FROM N TO NB
+        net_flow =
+            #JuMP.value.(sum(reactive[index] for index in generators_index[n]; init=0)) # Generators 
+            JuMP.value.(-v[n]^2 * B[n, nb] + v[n] * v[nb] * B[n, nb] * cos(θ[n] - θ[nb]) - v[n] * v[nb] * G[n, nb] * sin(θ[n] - θ[nb])) #  FROM N TO NB
+            - JuMP.value.(-v[nb]^2 * B[nb, n] + v[nb] * v[n] * B[nb, n] * cos(θ[nb] - θ[n]) - v[nb] * v[n] * G[nb, n] * sin(θ[nb] - θ[n])) #  FROM NB TO N
         println("From node $n to node $nb the net flow is $net_flow")
     end
 end
 
-
+for n in 1:n_len
+    println("for node $n")
+    for index in generators_index[n]
+        println(JuMP.value.(reactive[index]))
+    end
+end
 
 
